9f861c5968d1107706ec4d93bab28011633e80ba
mahdi abbasi <mahdiabbasi1100@gmail.com>
2020-04-20 15:29:31 +0430

    make project and build the navbar

---
+++ js/page1.js
@@ -0,0 +1,29 @@
+$(function () {
+    $("#menu").slideUp(0)
+    $("#open_menu").click(
+        function () {
+            $("#menu").slideToggle(1000);
+        }
+    )
+    $("a").click(
+        function () {
+            $("#menu").slideUp(1000)
+        }
+    )
+
+})
+//
+// var slideIndex = 0;
+// carousel();
+//
+// function carousel() {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1}
+//     x[slideIndex-1].style.display = "block";
+//     setTimeout(carousel, 2500);
+// }
