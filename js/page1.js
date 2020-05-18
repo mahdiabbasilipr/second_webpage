bb216ecff662bfa61860713f6bdc32ad41b7bccc
Your Name <you@example.com>
2020-05-18 17:55:50 +0430

    bad calender

--- js/page1.js
+++ js/page1.js
@@ -210,6 +210,21 @@ $('.owl-carousel').owlCarousel({
 
 });
 
-
-
+// FOR CALENDER
+$('#input1').change(function() {
+    var $this = $(this),
+        value = $this.val();
+    alert(value);
+});
+$('#textbox1').change(function () {
+    var $this = $(this),
+        value = $this.val();
+    alert(value);
+});
+$('[data-name="disable-button"]').click(function() {
+    $('[data-mddatetimepicker="true"][data-targetselector="#input1"]').MdPersianDateTimePicker('disable', true);
+});
+$('[data-name="enable-button"]').click(function () {
+    $('[data-mddatetimepicker="true"][data-targetselector="#input1"]').MdPersianDateTimePicker('disable', false);
+});
 
